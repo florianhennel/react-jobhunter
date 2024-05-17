5b99d62dbfa437cd21f7b4ea35c5fcdc4d6c7987
florianhennel <hennelflori@gmail.com>
2024-05-17 11:50:27 +0200

    added routing and redux store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./state/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            {/*<App />*/}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={App} />
+                    <Route path="login" element={Login}></Route>
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
+);
