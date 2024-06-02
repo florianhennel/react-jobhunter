89fbd2051f337767eb6162fda3f7b7683b86c385
florianhennel <hennelflori@gmail.com>
2024-06-02 12:18:45 +0200

    added isAuthenticated state

--- client/src/state/authSlice.js
+++ client/src/state/authSlice.js
@@ -6,25 +6,29 @@ export const authSlice = createSlice({
     initialState: {
         user: null,
         token: null,
+        isAuthenticated: false,
     },
     reducers: {
         login(state, {payload}) {
             state.user = payload.user;
             state.token = payload.accessToken;
+            state.isAuthenticated = true;
         },
         logout(state) {
             state.user = null;
             state.token = null;
+            state.isAuthenticated = false;
         }
     },
     extraReducers: (builder) => {
         builder.addMatcher(authApiSlice.endpoints.login.matchFulfilled, (state, {payload}) => {
             state.user = payload.user;
             state.token = payload.accessToken;
-            console.log(payload.user);
+            state.isAuthenticated = true;
         })
     }
 })
+
 export const selectUser = (state) =>{
     return state.auth.user;
 }
