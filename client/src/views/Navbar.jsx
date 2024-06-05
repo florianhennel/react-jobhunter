bcf8d553636eddb2e67ce2a1e875b4817e26318e
florianhennel <hennelflori@gmail.com>
2024-06-05 18:24:35 +0200

    handling logout

--- client/src/views/Navbar.jsx
+++ client/src/views/Navbar.jsx
@@ -1,9 +1,15 @@
-import { useSelector } from "react-redux"
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../state/authSlice";
 
 export default function Navbar(){
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     const isAuthenticated = useSelector((state)=>state.auth.isAuthenticated);
     const role = isAuthenticated?useSelector((state)=>state.auth.user.role):null;
+    const handleLogout = ()=>{
+        dispatch(logout());
+    }
     return(
         <div className="navbar bg-neutral flex pl-4 gap-2 w-full">
             <Link className="btn btn-ghost text-xl">Jobhunter</Link>
@@ -14,7 +20,7 @@ export default function Navbar(){
             <div className={`${isAuthenticated?"visible":"hidden"}`}>
                 <Link className="btn btn-ghost text-xl">Profil</Link>
                 <Link className={`btn btn-ghost text-xl ${role === "company"?"visible":"hidden"}`}>Álláshirdetés</Link>
-                <Link className="btn btn-ghost text-xl">Kijelentkezés</Link>
+                <Link className="btn btn-ghost text-xl" onClick={handleLogout}>Kijelentkezés</Link>
             </div>
             
         </div>
