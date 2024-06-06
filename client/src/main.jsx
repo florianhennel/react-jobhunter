14987e61d759de78f2608f8f07b32fed108513d7
florianhennel <hennelflori@gmail.com>
2024-06-06 18:26:00 +0200

    added new routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"
+import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./state/store.js";
 import Login from "./views/Login.jsx";
 import RequireAuth from "./RequireAuth.jsx";
+import Register from "./views/Register.jsx";
+import Profil from "./views/Profil/Profil.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<App/>} />
-                    <Route path="/login" element={<Login/>} />   
-                </Routes>
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profil" element={<Profil />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
