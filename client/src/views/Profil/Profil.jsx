b5cc3b46d32b93f2373edaa69f17499a4e7a2819
florianhennel <hennelflori@gmail.com>
2024-06-11 11:54:07 +0200

    changed structure and names of subcomponents

--- client/src/views/Profil/Profil.jsx
+++ client/src/views/Profil/Profil.jsx
@@ -1,59 +1,87 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Navbar from "../Navbar";
-import { useDispatch, useSelector } from "react-redux";
-import Data from "./ProfileData";
+import { useSelector } from "react-redux";
+import ProfileData from "./ProfileData";
 import { useGetExperienceQuery } from "../../state/experienceApiSlice";
-import JobData from "./ExperienceData";
-import { editExperiences } from "../../state/experineceSlice";
+import ExperienceData from "./ExperienceData";
+import CreateJobForm from "../Job/CreateJobForm";
+import {
+  useCreateJobMutation,
+  useGetFilteredJobsQuery,
+} from "../../state/jobsApiSlice";
+import OwnJob from "./OwnJob";
 
 export default function Profil() {
   const { id, email, fullname, role } = useSelector((state) => state.auth.user);
   const token = useSelector((state) => state.auth.token);
-  const [edit,setEdit] = useState(false);
+  const [edit, setEdit] = useState(false);
   const {
     data: { data, limit, skip, total } = {},
     isLoading,
     isError,
     isSuccess,
   } = useGetExperienceQuery({ token: token });
-  const dispatch = useDispatch();
-  const exp = useSelector(state=>state.experience.experiences)
-  useEffect(()=>{
-    dispatch(editExperiences({data}));
-  },[data])
+
+  const jobs = useGetFilteredJobsQuery({ userId: id });
+  const [apiCreateJob] = useCreateJobMutation();
+
   return (
     <div className="w-full h-screen">
       <Navbar />
-      <div className=" flex justify-center items-center h-full">
-        <div className="card bg-neutral shadow-xl w-1/2">
-          <div className="card-body">
-            <div className=" flex justify-between">
-              <h2 className="card-title">Személyes adatok</h2>
-            </div>
+      <div className=" flex justify-center items-start h-full pt-8">
+        {role === "jobseeker" ? (
+          <div className="card bg-neutral shadow-xl w-1/2">
+            <div className="card-body">
+              <div className=" flex justify-between">
+                <h2 className="card-title">Személyes adatok</h2>
+              </div>
 
-            <div className="divider"></div>
-            <div className=" flex flex-col items-start gap-4">
-              <Data title={"Név"} />
-              <Data title={"Email"} />
-              <Data title={"Státusz"} />
-            </div>
-            <br />
-            <div>
-              <h2 className="card-title">Previous experiences</h2>
               <div className="divider"></div>
-              {data ? (
-                <div className=" flex flex-col items-start gap-1">
-                  <JobData job={{company:"Company",interval:"Interval",title:"Position"}} />
-                  {data.map((j, index) => (
-                    <JobData key={index} job={j} edit={edit} />
-                  ))}
-                </div>
-              ) : (
-                "No experiences"
-              )}
+              <div className=" flex flex-col items-start gap-4">
+                <ProfileData title={"Név"} />
+                <ProfileData title={"Email"} />
+                <ProfileData title={"Státusz"} />
+              </div>
+              <br />
+              <div>
+                <h2 className="card-title">Previous experiences</h2>
+                <div className="divider"></div>
+                {data ? (
+                  <div className=" flex flex-col items-start gap-1">
+                    <ExperienceData
+                      job={{
+                        company: "Company",
+                        interval: "Interval",
+                        title: "Position",
+                      }}
+                    />
+                    {data.map((j, index) => (
+                      <ExperienceData key={index} job={j} edit={edit} />
+                    ))}
+                  </div>
+                ) : (
+                  "No experiences"
+                )}
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          jobs.isSuccess && (
+            <div className=" flex flex-col gap-4 w-1/2 justify-center items-center">
+              {jobs.data.data.map((j, index) => (
+                <OwnJob key={index} details={j} />
+              ))}
+              <CreateJobForm details={null}/>
+              <button className=" btn btn-primary"
+                onClick={() =>
+                  document.getElementById("create_job_modal").showModal()
+                }
+              >
+                Hirdetés hozzáadása
+              </button>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
