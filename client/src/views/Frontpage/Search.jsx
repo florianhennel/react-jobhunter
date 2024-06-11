f78376138b048b32c416bba0ecbba6917d6a33d0
florianhennel <hennelflori@gmail.com>
2024-06-11 11:49:48 +0200

    added search value to filters

--- client/src/views/Frontpage/Search.jsx
+++ client/src/views/Frontpage/Search.jsx
@@ -1,30 +1,49 @@
+import { useDispatch, useSelector } from "react-redux";
 import Filter from "./Fitler";
+import { useState } from "react";
+import { setFilters } from "../../state/filterSlice";
 export default function Search() {
-  const handleSearch = ()=>{
-
-  }
-  return (
-    <div className="flex flex-col gap-2 justify-center w-1/2">
-      <div className=" card-title">Böngéssz az állások között:</div>
-      <div className="flex gap-2 justify-start w-full">
-        <label className="input input-bordered flex items-center gap-2 w-full">
-          <input type="text" className="grow w-full" placeholder="Search" />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </label>
-        <button className="btn btn-primary" onClick={handleSearch}>Search</button>
-        <Filter />
-      </div>
-    </div>
-  );
+  const dispatch =useDispatch();
+  const filters = useSelector((state) => state.filter);
+  const [input,setInput] = useState("");
+    const handleSearch = () => {
+        dispatch(
+            setFilters({
+                ...filters,
+                title:input
+            })
+        );
+    };
+    return (
+        <div className="flex flex-col gap-2 justify-center w-1/2">
+            <div className=" card-title">Böngéssz az állások között:</div>
+            <div className="flex gap-2 justify-start w-full">
+                <label className="input input-bordered flex items-center gap-2 w-full">
+                    <input
+                        type="text"
+                        className="grow w-full"
+                        placeholder="Search"
+                        value={input}
+                        onInput={(event)=>setInput(event.target.value)}
+                    />
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="w-4 h-4 opacity-70"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                </label>
+                <button className="btn btn-primary" onClick={handleSearch}>
+                    Search
+                </button>
+                <Filter />
+            </div>
+        </div>
+    );
 }
