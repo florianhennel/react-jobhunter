730731d39cd1cd10901c8081868d270e66792891
florianhennel <hennelflori@gmail.com>
2024-06-11 11:46:40 +0200

    addded queries and tags

--- client/src/state/jobsApiSlice.js
+++ client/src/state/jobsApiSlice.js
@@ -1,54 +1,108 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const jobsApiSlice = createApi({
-    reducerPath: "jobsApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:3030/jobs",
+  reducerPath: "jobsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3030/jobs",
+  }),
+  tagTypes: ["jobs"],
+  endpoints: (builder) => ({
+    getAllJobs: builder.query({
+      query: () => "",
     }),
-    tagTypes:['jobs'],
-    endpoints: (builder)=>({
-        getAllJobs:builder.query({
-            query: ()=> "",   
+    getOneJob: builder.query({
+      query: ({ id }) => `${id}`,
+    }),
+    getFilteredJobs: builder.query({
+      query: (filters) => filterBuilder(filters),
+      providesTags: ["jobs"],
+    }),
+    modifyJob: builder.mutation({
+      query: ({ id, body, token }) => ({
+        method: "PATCH",
+        url: `${id}`,
+        headers: {
+          "Content-type": "application/json",
+          "Accept": "application/json",
+          "Authorization": "Bearer " + token,
+        },
+        body: body,
+      }),
+      invalidatesTags:['jobs']
+    }),
+    deleteJob:builder.mutation({
+        query: ({token, id}) => ({
+            method:"DELETE",
+            url:`${id}`,
+            headers: {
+                "Content-type": "application/json",
+                "Accept": "application/json",
+                "Authorization": "Bearer " + token,
+              },
         }),
-        getOneJob:builder.query({
-            query: ({id})=> `${id}`,   
+        invalidatesTags:['jobs']
+    }),
+    createJob:builder.mutation({
+        query: ({token,body})=>({
+            method:"POST",
+            url:"/1",
+            headers: {
+                "Content-type": "application/json",
+                "Accept": "application/json",
+                "Authorization": "Bearer " + token,
+              },
+            body:body
         }),
-        getFilteredJobs:builder.query({
-            query:(filters)=>filterBuilder(filters),
-            providesTags:['jobs']
-        })
+        invalidatesTags:['jobs']
     })
-})
-const filterBuilder = (filters)=>{
-    let string = "?";
-    if (filters.minWage !== null) {
-        string+=`salaryFrom[$gte]=${filters.minWage*20000}&`
-    }
-    if (filters.maxWage !== null) {
-        string+=`salaryTo[$lte]=${filters.maxWage*20000}&`
-    }
-    if (filters.title !== "") {
-        string+=`position[$like]=%${filters.title}%&`
-    }
-    if (filters.jobType !== "Összes") {
-        string+=`type=${(filters.jobType).toLowerCase()}&`
-    }
-    if (filters.location !== "") {
-        string+=`city=${filters.location}&`
-    }
-    if (filters.homeOffice !== null) {
-        string+=`homeOffice=${filters.homeOffice}&`
-    }
-    if (filters.userId !== null) {
-        string+=`userId=${filters.userId}&`
-    }
-    if (filters.company !== "") {
-        string+=`company[$like]=%${filters.company}%&`
-    }
-    if (string.length===1) {
-        return "";
-    }else{
-        return string.substring(0,(string.length-1));
-    }
-}
-export const { useGetAllJobsQuery, useGetFilteredJobsQuery, useGetOneJobQuery } = jobsApiSlice;
+  }),
+});
+const filterBuilder = ({
+  minWage,
+  maxWage,
+  title,
+  jobType,
+  location,
+  homeOffice,
+  userId,
+  company,
+}) => {
+  let string = "?";
+  if (minWage) {
+    string += `salaryFrom[$gte]=${minWage * 20000}&`;
+  }
+  if (maxWage) {
+    string += `salaryTo[$lte]=${maxWage * 20000}&`;
+  }
+  if (title) {
+    string += `position=${title}&`;
+  }
+  if (jobType !== "Összes" && jobType) {
+    string += `type=${jobType.toLowerCase()}&`;
+  }
+  if (location) {
+    string += `city=${location}&`;
+  }
+  if (homeOffice) {
+    string += `homeOffice=${homeOffice}&`;
+  }
+  if (userId) {
+    string += `userId=${userId}&`;
+  }
+  if (company) {
+    string += `company[$like]=%${company}%&`;
+  }
+  if (string.length === 1) {
+    return "";
+  } else {
+    return string.substring(0, string.length - 1);
+  }
+};
+export const {
+  useGetAllJobsQuery,
+  useGetFilteredJobsQuery,
+  useGetOneJobQuery,
+  useModifyJobMutation,
+  useDeleteJobMutation,
+  useCreateJobMutation
+} = jobsApiSlice;
--- client/src/state/jobsSlice.js
+++ client/src/state/jobsSlice.js
@@ -1,35 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit"
-import { jobsApiSlice } from "./jobsApiSlice"
+import { createSlice } from "@reduxjs/toolkit";
+import { jobsApiSlice } from "./jobsApiSlice";
 
 export const jobsSlice = createSlice({
-    name:"jobs",
-    initialState: {
-        total: 0,
-        limit: 10,
-        skip: 0,
-        data: [],
+  name: "jobs",
+  initialState: {
+    total: 0,
+    limit: 10,
+    skip: 0,
+    data: [],
+  },
+  reducers: {
+    getAllJobs(state, { payload }) {
+      state.total = payload.total;
+      state.limit = payload.limit;
+      state.skip = payload.skip;
+      state.data = payload.data;
     },
-    reducers:{
-        getAllJobs(state, {payload}) {
-            state.total = payload.total;
-            state.limit = payload.limit;
-            state.skip = payload.skip;
-            state.data = payload.data;
-        },
-    },
-    extraReducers: (builder)=>{
-        builder.addMatcher(jobsApiSlice.endpoints.getAllJobs.matchFulfilled, (state, {payload})=>{
-            state.total = payload.total;
-            state.limit = payload.limit;
-            state.skip = payload.skip;
-            state.data = payload.data;
-        }),
-        builder.addMatcher(jobsApiSlice.endpoints.getFilteredJobs.matchFulfilled,(state, {payload})=>{
-            state.total = payload.total;
-            state.limit = payload.limit;
-            state.skip = payload.skip;
-            state.data = payload.data;
-        })
-    }
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      jobsApiSlice.endpoints.getAllJobs.matchFulfilled,
+      (state, { payload }) => {
+        state.total = payload.total;
+        state.limit = payload.limit;
+        state.skip = payload.skip;
+        state.data = payload.data;
+      }
+    ),
+      builder.addMatcher(
+        jobsApiSlice.endpoints.getFilteredJobs.matchFulfilled,
+        (state, { payload }) => {
+          state.total = payload.total;
+          state.limit = payload.limit;
+          state.skip = payload.skip;
+          state.data = payload.data;
+        }
+      );
+  },
+});
 
-})
+export const { inputErrors } = jobsSlice.actions;
